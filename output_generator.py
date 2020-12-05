580bf9524f56c2229f0692fd1c30fdb8a468bedb
Aadarsh Singh <aadarshsingh191198@gmail.com>
2020-12-05 08:29:47 +0530

    Add function to generate predictions from logits

--- output_generator.py
+++ output_generator.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import numpy as np
 from tqdm import tqdm
 import sys
 def fixed_tags(tags):
@@ -21,6 +22,16 @@ def fixed_tags(tags):
     assert len(fixed) == len(tags)
     return fixed
 
+def softmax(x):
+    return np.exp(x)/sum(np.exp(x))
+
+def logit_to_preds(logits,mask,labels=["O","U-long","U-short"]):
+    soft_logit = [softmax(logit) for logit in logits]
+    max_logit = [np.argmax(logit) for logit in soft_logit]
+    masked_logit = max_logit[:sum(mask)]
+    logit_predictions = [labels[i] for i in masked_logit]
+    return logit_predictions
+
 def naive_fixed_tags(tags):
     fixed_tags_dict = {"U-short":"B-short","U-long":"B-long","L-short":"I-short","L-long":"I-long"}
     return [fixed_tags_dict.get(tag, tag) for tag in tags]
@@ -33,8 +44,11 @@ if __name__ == '__main__':
     preds = []
     for i,prediction in tqdm(enumerate(predictions)):
         prediction = json.loads(prediction)
-        preds.append({"id":f"TS-{i}", "predictions":fixed_tags(prediction['tags'])})
+        logit_preds = logit_to_preds(prediction['logits'], prediction['mask'])
+        preds.append({"id":f"TS-{i}", "predictions":fixed_tags(logit_preds)})
         # preds.append({"id":f"TS-{i}", "predictions":naive_fixed_tags(prediction['tags'])})
+        # preds.append({"id":f"TS-{i}", "predictions":fixed_tags(prediction['tags'])})
+
         
     f = open(sys.argv[2],'w') #output.json
     f.write(str(preds).replace("'",'"'))
